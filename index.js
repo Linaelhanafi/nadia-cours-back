795278262e089a09da01d502e42cd7b5b5284ba8
lina <linalinahanafi@gmail.com>
2023-08-07 14:28:36 +0100

    done

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
-app.use(cors({ origin: 'www.nadiacours.com' }));
+const allowedOrigins = ['https://www.nadiacours.com', 'https://nadiacours.com'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 
 const transporter = nodemailer.createTransport({
